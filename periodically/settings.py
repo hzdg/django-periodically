c78b42202f54a1fb7031a9a45b73f78a5b05e3fd
Matthew Tretter <matthew@exanimo.com>
2011-06-02 16:52:30 -0400

    Fixed settings access.

--- periodically/settings.py
+++ periodically/settings.py
@@ -4,5 +4,5 @@ import datetime
 
 
 _settings = getattr(project_settings, 'PERIODICALLY_SETTINGS', {})
-DEFAULT_BACKEND = getattr(_settings, 'DEFAULT_BACKEND', CommandBackend)
-DEFAULT_TIMEOUT = getattr(_settings, 'DEFAULT_TIMEOUT', datetime.timedelta(minutes=10))
+DEFAULT_BACKEND = _settings.get('DEFAULT_BACKEND', DefaultBackend)
+DEFAULT_TIMEOUT = _settings.get('DEFAULT_TIMEOUT', datetime.timedelta(minutes=10))
