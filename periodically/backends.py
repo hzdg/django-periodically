4bf9f1c1c8f8b9159746e4d316640971907dc6a4
Matthew Tretter <matthew@exanimo.com>
2011-11-04 16:34:20 -0400

    PEP8

--- periodically/backends.py
+++ periodically/backends.py
@@ -8,7 +8,6 @@ import sys
 
 # FIXME: We really need to clarify our use of the word "scheduled." Does it mean scheduled with the backend (as in schedule_task) or scheduled to run (as in run_scheduled_tasks)
 
-
 class BaseBackend(object):
     """
     Keeps a schedule of periodic tasks.
@@ -19,25 +18,27 @@ class BaseBackend(object):
     @property
     def logger(self):
         return logging.getLogger('periodically') # TODO: Further namespace logger?
-    
+
     @property
     def tasks(self):
         """A list of the tasks scheduled with this backend."""
         return set([task for task, schedule in self._schedules])
-    
+
     def schedule_task(self, task, schedule):
         """
         Schedules a periodic task.
+
         """
         # Don't add the same one twice.
         for t, s in self._schedules:
             if t.task_id == task.task_id and s == schedule:
                 return
-        
+
         task_id = task.task_id
-        self.logger.info('Scheduling task %s to run on schedule %s' % (task_id, schedule))
+        self.logger.info('Scheduling task %s to run on schedule %s' % (task_id,
+                schedule))
         self._schedules.append((task, schedule))
-        
+
         # Subscribe to the task_complete signal. We do this when the task
         # is scheduled (instead of when it runs) so that if you kill Django
         # with unfinished tasks still running, they will be able to
@@ -45,8 +46,9 @@ class BaseBackend(object):
         # determined by polling a web service. When Django restarts, the
         # polling could start again and the task would be completed.)
         if not getattr(task, 'is_blocking', True):
-            task_complete.connect(self._create_receiver(task.__class__), sender=task.__class__, dispatch_uid=task_id)
-    
+            task_complete.connect(self._create_receiver(task.__class__),
+                    sender=task.__class__, dispatch_uid=task_id)
+
     def run_scheduled_tasks(self, tasks=None, fake=None):
         """
         Runs any scheduled periodic tasks and ends any tasks that have exceeded
@@ -60,7 +62,7 @@ class BaseBackend(object):
         Run tasks regardless of whether they are scheduled.
         """
         self._run_tasks(tasks, fake, True)
-    
+
     def _run_tasks(self, tasks=None, fake=None, force=False):
         now = datetime.now()
 
@@ -68,32 +70,34 @@ class BaseBackend(object):
         if tasks:
             for task in tasks:
                 if task.task_id not in set([task.task_id for task in self.tasks]):
-                    raise Exception('%s is not registered with this backend.' % task)
-        
+                    raise Exception('%s is not registered with this'
+                            ' backend.' % task)
+
         registered_task_ids = [task.task_id for task in tasks]
-        
+
         for task, schedule in self._schedules:
             if not tasks or task.task_id in registered_task_ids:
-                
+
                 # Cancel the task if it's timed out.
                 # FIXME: This should only be called once per task (no matter how many times it's scheduled).
                 self.check_timeout(task, now)
-                
+
                 # If there are still tasks running, don't run the queue (as we
                 # could mess up the order).
                 if ExecutionRecord.objects.filter(end_time__isnull=True):
                     print 'There are still tasks running; no new tasks will be run'
                     # TODO: Should this behave differently if force == True?
                     return
-            
+
                 # Run the task if it's due (or past due).
                 if force or get_scheduled_time(task, schedule, now) <= now:
                     previous_record = ExecutionRecord.objects.get_most_recent(task, schedule)
                     fake_task = fake or fake is None and previous_record is None
-                
+
                     # If we're forcing the task, use the previous scheduled
                     # time.
-                    scheduled_time = previous_record.scheduled_time if force and previous_record else None
+                    scheduled_time = previous_record.scheduled_time if force \
+                            and previous_record else None
 
                     if fake_task:
                         self.fake_task(task, schedule, scheduled_time, now)
@@ -116,7 +120,7 @@ class BaseBackend(object):
                     'msg': 'Task timed out after %s.' % running_time,
                 }
                 self.complete_task(task, False, extra=extra)
-    
+
     def check_timeouts(self, now=None):
         """
         Checks to see whether any scheduled tasks have timed out and handles
@@ -124,7 +128,7 @@ class BaseBackend(object):
         """
         for task in self.tasks:
             self.check_timeout(task, now)
-    
+
     def fake_task(self, task, schedule, scheduled_time=None, now=None):
         # TODO: Do we need both of these?
         print 'Faking periodic task "%s"' % task.task_id
@@ -132,7 +136,7 @@ class BaseBackend(object):
 
         if scheduled_time is None:
             scheduled_time = get_scheduled_time(task, schedule, now)
-        
+
         # Create the log for this execution.
         log = ExecutionRecord.objects.create(
             task_id=task.task_id,
@@ -140,8 +144,9 @@ class BaseBackend(object):
             scheduled_time=scheduled_time,
             start_time=now,
             end_time=now,
-            is_fake=True,)
-    
+            is_fake=True,
+        )
+
     def run_task(self, task, schedule, scheduled_time=None, now=None):
         """
         Runs the provided task. This method is provided as a convenience to
@@ -155,18 +160,18 @@ class BaseBackend(object):
         # TODO: Do we need both of these?
         print 'Running periodic task "%s"' % task.task_id
         self.logger.info('Running periodic task "%s"' % task.task_id)
-        
-        
+
         if scheduled_time is None:
             scheduled_time = get_scheduled_time(task, schedule, now)
-        
+
         # Create the log for this execution.
         log = ExecutionRecord.objects.create(
             task_id=task.task_id,
             schedule_id=schedule.__hash__(),
             scheduled_time=scheduled_time,
             start_time=now,
-            end_time=None,)
+            end_time=None,
+        )
 
         # Run the task.
         try:
@@ -191,13 +196,14 @@ class BaseBackend(object):
         finally:
             if not success or getattr(task, 'is_blocking', True):
                 self.complete_task(task, success, extra=extra)
-    
+
     def _create_receiver(self, sender):
         def receiver(task, extra=None):
-            task_complete.disconnect(receiver, sender, dispatch_uid=task.task_id)
+            task_complete.disconnect(receiver, sender,
+                    dispatch_uid=task.task_id)
             self.complete_task(task, True, extra=extra)
         return receiver
-    
+
     def complete_task(self, task, success=True, extra=None):
         """
         Marks a task as complete and performs other post-completion tasks. The
@@ -207,7 +213,7 @@ class BaseBackend(object):
         """
         if extra is not None:
             self.logger.log(**extra)
-            
+
         record = ExecutionRecord.objects \
                 .filter(task_id=task.task_id, end_time=None) \
                 .order_by('-start_time')[0]
@@ -216,7 +222,6 @@ class BaseBackend(object):
         record.save()
 
         # TODO: Retries.
-
 
 class DefaultBackend(BaseBackend):
     """
