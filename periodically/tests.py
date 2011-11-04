ca7d44b1862df6472419dad37067b4aef1fda737
Matthew Tretter <matthew@exanimo.com>
2011-11-04 17:04:46 -0400

    First test

--- periodically/tests.py
+++ periodically/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from . import schedules
+from datetime import datetime
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+now = datetime(1983, 7, 1, 3, 41)
+
+
+class ScheduleTest(TestCase):
+    def test_hourly(self):
+        sched = schedules.Hourly(20, 2, 4)
+        self.assertEqual(sched.time_before(now), datetime(1983, 7, 1, 3, 20, 2, 4))
+        self.assertEqual(sched.time_after(now), datetime(1983, 7, 1, 4, 20, 2, 4))
