b6fa47b0a2195c754c0770316af5130cdad4f8af
Matthew Tretter <matthew@exanimo.com>
2011-11-04 18:32:04 -0400

    Test for `Every`

--- periodically/tests.py
+++ periodically/tests.py
@@ -11,3 +11,8 @@ class ScheduleTest(TestCase):
         sched = schedules.Hourly(20, 2, 4)
         self.assertEqual(sched.time_before(now), datetime(1983, 7, 1, 3, 20, 2, 4))
         self.assertEqual(sched.time_after(now), datetime(1983, 7, 1, 4, 20, 2, 4))
+
+    def test_every(self):
+        sched = schedules.Every(minutes=1)
+        self.assertEqual(sched.time_before(now), datetime(1983, 7, 1, 3, 41))
+        self.assertEqual(sched.time_after(now), datetime(1983, 7, 1, 3, 42))
